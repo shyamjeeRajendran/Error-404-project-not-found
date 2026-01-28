a0bb0c42acbe83bbd5f106c479f47651b368c11b
shyamjeeRajendran <shyamspace08@gmail.com>
2026-01-29 01:56:35 +0530

    Initial commit

--- main.js
+++ main.js
@@ -11,3 +11,32 @@ menu.addEventListener('click',()=>{
 x.addEventListener('click',()=>{
     sideMenu.style.right= '-100%';
 })
+
+// hero section 
+const slides = document.querySelectorAll('.slide');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+let current = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) slide.classList.add('active');
+  });
+}
+
+// Next 
+function nextSlide() {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}
+
+// Previous 
+function prevSlide() {
+  current = (current - 1 + slides.length) % slides.length;
+  showSlide(current);
+}
+
+// Event listener
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
