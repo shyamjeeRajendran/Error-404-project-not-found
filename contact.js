0791e671cfbf9d85de88e8ffa7b90a99d5366df1
aruna9603 <arunaanbazhagan03@gmail.com>
2026-01-31 03:00:47 +0900

    updated Contact us page

---
+++ contact.js
@@ -0,0 +1,62 @@
+const uparrow = document.getElementById("arrow")
+
+arrow.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+var submit = document.querySelector(".submit")
+
+submit.addEventListener("click", function () {
+  event.preventDefault()
+
+  var Regexname = /^[A-Za-z]+$/
+  var Regexemail = /^[a-zA-Z0-9]+@gmail\.com$/
+  var Regexphone = /^\d{10}$/
+
+  const name = document.getElementById("name")
+  const email = document.getElementById("email")
+  const phone = document.getElementById("phone")
+  const textarea = document.getElementById("message")
+  const count = document.getElementById("count")
+  const error = document.querySelector(".messageerror")
+
+  const maxLength = 200;
+
+
+  if (Regexname.test(name.value) == false) {
+    document.querySelector(".nameerror").style.display = "inline"
+  }
+  else {
+    document.querySelector(".nameerror").style.display = "none"
+  }
+  if (Regexemail.test(email.value) == false) {
+    document.querySelector(".emailerror").style.display = "inline"
+  }
+  else {
+    document.querySelector(".emailerror").style.display = "none"
+  }
+  if (Regexphone.test(phone.value) == false) {
+    document.querySelector(".phoneerror").style.display = "inline"
+  }
+  else {
+    document.querySelector(".phoneerror").style.display = "none"
+  }
+
+  const length = textarea.value.length;
+
+  if (length > maxLength == true) {
+    error.style.display = "inline"
+  }
+  else {
+    error.style.display = "none"
+  }
+
+  textarea.addEventListener("input", function () {
+    count.innerHTML = textarea.value.length + "/200 characters";
+  });
+
+
+})
