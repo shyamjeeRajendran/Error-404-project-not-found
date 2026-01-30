c1c1295dd45cb4c35be09da37a38387c4d3db7ff
shyamjeeRajendran <shyamspace08@gmail.com>
2026-01-31 01:35:23 +0530

    upade catalog menu bar

--- catalogue.js
+++ catalogue.js
@@ -79,3 +79,18 @@ form.addEventListener('submit',(e)=>{
   form.reset()
 })
 
+// sidebar slide 
+const sideMenu = document.querySelector('.side-menu');
+const menu = document.querySelector('.menu-bar');
+const x = document.querySelector('#x-mark');
+const sign_in = document.querySelector('.signin-side');
+
+menu.addEventListener('click',()=>{
+ sideMenu.style.right= '0';
+})
+
+x.addEventListener('click',()=>{
+    sideMenu.style.right= '-100%';
+})
+
+
