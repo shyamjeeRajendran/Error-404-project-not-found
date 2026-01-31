a9594e55d05d1a36306428cc0a194d6ac23015dd
aruna9603 <arunaanbazhagan03@gmail.com>
2026-02-01 01:22:39 +0900

    updated contact.js file

--- contact.js
+++ contact.js
@@ -22,6 +22,8 @@ submit2.addEventListener("click", function () {
   const name = document.getElementById("name")
   const email = document.getElementById("email")
   const phone = document.getElementById("phone")
+  const textarea = document.getElementById("message")
+  const count = document.getElementById("count")
   const error = document.querySelector(".messageerror")
 
   const maxLength = 200;
